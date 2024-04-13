cd8b903122bad454dd46a56053e4723b6ef6795d
aknorsh <aknorsh@gmail.com>
2024-04-13 19:48:04 +0900

    go fmt

--- internal/upperid/testdata/src/a/a.go
+++ internal/upperid/testdata/src/a/a.go
@@ -1,9 +1,9 @@
 package a
 
 func f() {
-	var ID int // IDになってるのでOK
+	var ID int  // IDになってるのでOK
 	println(ID) // IDになってるのでOK
 
-	var Id int // want "Id must be ID"
+	var Id int  // want "Id must be ID"
 	println(Id) // want "Id must be ID"
 }
